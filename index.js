5a36ae543146671236046644187a1a306c96088b
DSmithMr <dsmith.mr@gmail.com>
2022-08-16 22:41:00 -0500

    Added html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 
 const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
+app.use(express.static(__dirname + "/public"));
+
 io.on("connection", () => {
-  /* … */
+  console.log("Cliente conectado");
 });
 
 server.listen(8080, () => {
